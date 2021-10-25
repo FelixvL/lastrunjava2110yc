8912f2a72df78980c9362956f1e3419a4d97b676
felixvl <digitaleservice@gmail.com>
2021-10-25 16:34:27 +0200

    SP

--- Demo.java
+++ Demo.java
@@ -1,22 +1,24 @@
-class Demo{   // SO
+class Demo{   // SP
 	public static void main(String... ab){
-		Q q = new E();
-		L l = new E();
-		q.i();
+		Q q = new E();    // links bepaalt waar je bij kan 
+					// rechts bepaalt wat je uiteindelijk kan en doet
+		E l = new E();
+		l.i();
 		System.out.println("ready");
 	}
 }
 
-abstract class Q{		
+abstract class Q{   
+		
 	
 }
 
-class E extends Q implements L{           
+class E extends Q implements L{               
 	void i(){
 		System.out.println("in E de i");
 	}
 }
 
-interface L{       
-	void i();
+interface L{    					   
+	void i();			
 }
