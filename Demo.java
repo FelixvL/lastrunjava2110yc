48c8f2dbbfd14e6eb8ec66e1f83d9ce933e67665
felixvl <digitaleservice@gmail.com>
2021-10-25 15:41:20 +0200

    SA

--- Demo.java
+++ Demo.java
@@ -1,6 +1,16 @@
-class Demo{   // RK
+class Demo{   // SA
 	public static void main(String... ab){
-
+		Kip kap = new Kip();
+		kap.herewego();
+		System.out.println("tok");
 	}
 }
 
+class Kip{
+	boolean boos;
+	static int herewego(){
+		System.out.println("de kip is boos? " + boos);
+		return 9;
+	}
+
+}
