8c2216f7797f2d78260dea4e529a83a9931e4f46
felixvl <digitaleservice@gmail.com>
2021-10-27 14:43:31 +0200

    NU

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
-class Demo{   // NT   
+class Demo{   // NU   
 	final static public void main(String[] ab){ 
-		System.out.println("he"+ab[3]);
+		System.out.println("he");
 		oei();
 	}
-	void oei(int... daargaanwe){
+	static void oei(int... daargaanwe){
 		System.out.println(daargaanwe[daargaanwe.length-1]);
 	}
 }
