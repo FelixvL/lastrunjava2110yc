576ea00cf1743d6e58c1a06412ae59ce2a675e5e
felixvl <digitaleservice@gmail.com>
2021-10-27 11:16:48 +0200

    NA

--- Demo.java
+++ Demo.java
@@ -1,4 +1,16 @@
-class Demo{   // LEEG
+class Demo{   // NA
 	public static void main(String... ab){
+		System.out.println("start");
+		beginnen();
 	}
+	void beginnen(){
+		Go no = new Go();
+		Go so = new Go();
+		no.a = 12;
+		System.out.println(so.a);
+	}
+}
+
+class Go{
+	static int a = 2;
 }
