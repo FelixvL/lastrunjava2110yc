07fa82b1b0ec6b49c2e4ccc0dcd7ad29189de37e
felixvl <digitaleservice@gmail.com>
2021-10-27 14:34:38 +0200

    NR

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
-class Demo{   // NP   java Demo a b c d e 
+class Demo{   // NR   java Demo a b c d e 
 	public static void main(String[] ab){ 
-		System.out.println("he"+ab.length);
+		System.out.println("he"+ab[ab.length]);
 	}
 }
