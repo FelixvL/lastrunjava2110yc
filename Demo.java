6fcfcb1165d2a5a54fb7c0f08df56443e539cb31
felixvl <digitaleservice@gmail.com>
2021-10-27 11:39:07 +0200

    NK

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
-class Demo{   // NJ
-	public static void main(String... ab){
+class Demo{   // NK
+	public static void main(String... ab) throws Exception{
 		System.out.println("ei");
 		Anders hetzelfde = new Anders();
 		hetzelfde.hup();
+		System.out.println("einde");
 	}
 }
 
