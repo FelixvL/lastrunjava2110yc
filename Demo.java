03b1ed390225522fd5ae95801a5781e46a25bb78
felixvl <digitaleservice@gmail.com>
2021-10-27 15:09:31 +0200

    NZ

--- Demo.java
+++ Demo.java
@@ -1,23 +1,24 @@
-class Demo{   // NY   
+class Demo{   // NZ   
 	final static public void main(String[] ab){ 
 		System.out.println("ke");
-		A a = new B();
-		a.gaan(3);
+		B a = new B();				
+		a.gaan(3);				
 		System.out.println("ko");
 	}
 }
-class A implements C{
-	public void gaan(){
+class A implements C{				
+	public void gaan(){			
 		System.out.println("gaan in a");
 	}
 }
 
-class B extends A{
-	public void gaan(int a){
+class B extends A{				
+	public int gaan(int a){		
 		System.out.println("gaan in b");
+		return 7;
 	}
 }
-interface C{
+interface C{				
 	int d = 5;
-	void gaan();
+	void gaan();				
 }
