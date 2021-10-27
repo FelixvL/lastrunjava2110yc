44924e1656dbffdf1943168d49b0b30a7a43a684
felixvl <digitaleservice@gmail.com>
2021-10-27 14:32:27 +0200

    NQ

--- Demo.java
+++ Demo.java
@@ -1,28 +1,5 @@
-class Demo{   // NP
-	public static void main(String... ab){ 
-		System.out.println("ei");
-		Anders hetzelfde = new Anders();
-		try{
-			hetzelfde.hup();
-		}catch(Throwable e){
-			System.out.println("cc");
-		}finally{
-			System.out.println("ee");
-		}
-		System.out.println("einde");
+class Demo{   // NQ
+	public static void main(String ab...){ 
+		System.out.println("he");
 	}
-}
-
-class Anders implements H{
-	void hup(){
-		throw new Error();
-	}
-	Object gaan(){
-		H h = new Anders();
-		return h;
-	}
-}
-
-interface H{
-
 }
