be2a34eda393b892f74244f2eb1b61088c248cb1
felixvl <digitaleservice@gmail.com>
2021-10-27 11:22:57 +0200

    NB

--- Demo.java
+++ Demo.java
@@ -1,16 +1,16 @@
-class Demo{   // NA
+class Demo{   // NB
 	public static void main(String... ab){
-		System.out.println("start");
-		beginnen();
+		System.out.println("begin");
+		new Demo().beginnen();
 	}
 	void beginnen(){
 		Go no = new Go();
 		Go so = new Go();
-		no.a = 12;
+		no.a = 15;
 		System.out.println(so.a);
 	}
 }
 
 class Go{
-	static int a = 2;
+	static int a = 5;
 }
