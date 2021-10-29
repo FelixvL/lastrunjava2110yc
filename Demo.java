9ad86a68f06a946a9ccfe0de31c89a76c1f72577
felixvl <digitaleservice@gmail.com>
2021-10-29 13:28:25 +0200

    OG

--- Demo.java
+++ Demo.java
@@ -1,25 +1,12 @@
-class Demo{   // OD   
-	final static public void main(String[] ab){ 
-		System.out.println("ke");
-		B a = new B();				
-		a.gaan();				
-		System.out.println("ko");
-	}
-}
-class A implements C{				
-	public B gaan(){			
-		System.out.println("gaan in a");
-		return new B();
+class Demo{   // OG  
+	final static public void main(String[] ab) { 
+		HereWeGo go = new HereWeGo();
+		go.gaan();
 	}
 }
 
-class B extends A{				
-	public B gaan(){		
-		System.out.println("gaan in b");
-		return null;
-	}
-}
-interface C{				
-	int d = 5;
-	B gaan() throws Exception;				
+
+
+class HereWeGo{
+	abstract void gaan();
 }
