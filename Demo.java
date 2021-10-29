6d6594b537b08f4b72e66ae90c72a83180e2e3b1
felixvl <digitaleservice@gmail.com>
2021-10-29 13:34:33 +0200

    OJ

--- Demo.java
+++ Demo.java
@@ -1,6 +1,6 @@
-class Demo{   // OI 
+class Demo{   // OJ
 	final static public void main(String[] ab) { 
-		HereWeGo go = null;
+		HereWeGo go = new Rechthoek();
 		go.hup();
 	}
 }
@@ -11,5 +11,16 @@ abstract class HereWeGo{
 	abstract void gaan();
 	static void hup(){
 		System.out.println("hup");
+	}
+}
+class Rond extends HereWeGo{
+
+}
+abstract class Vierkant extends Rond{
+
+}
+class Rechthoek extends Vierkant{
+	static void hup(){
+		System.out.println("huphup");
 	}
 }
