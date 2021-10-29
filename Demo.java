4e6932b39e5a8aaa16e173d82f2074da31156400
felixvl <digitaleservice@gmail.com>
2021-10-29 13:43:52 +0200

    OM

--- Demo.java
+++ Demo.java
@@ -1,30 +1,37 @@
-class Demo{   // OL
+class Demo{   // OM
 	final static public void main(String[] ab) { 
 		HereWeGo go = new Rechthoek();
 		go.hup();
 		go.gaan();
-		Rond r = (Rond)go;
+		System.out.println(go.a);
+		Rond r = go;
 		r.gaan();
+		r.hup();
+		System.out.println(go.a);
 	}
 }
 
 
 
 abstract class HereWeGo{
+	int a = 3;
 	abstract void gaan();
 	static void hup(){
 		System.out.println("hup");
 	}
 }
 abstract class Rond extends HereWeGo{
+	int a = 4;
 	void gaan(){
 		System.out.println("we zijn aan het gaan");
 	}
 }
 abstract class Vierkant extends Rond{
+	int a = 5;
 	abstract void gaan();
 }
 class Rechthoek extends Vierkant{
+	int a = 6;
 	void gaan(){
 		System.out.println("gaan bij rechthoek");
 	}
