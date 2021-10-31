7e4b18d9a1730d6829894b1fbd11205e3c9549ee
felixvl <digitaleservice@gmail.com>
2021-10-31 12:23:18 +0100

    PZ

--- Demo.java
+++ Demo.java
@@ -1,8 +1,9 @@
-class Demo{   // PW
+class Demo{   // PZ
 	final static public void main(String[] ab){ 
 		int a = 'e';
-		char c = a;
-		System.out.println(c);
+		char c = (char)a;
+		long d = new Integer("2");
+		System.out.println(d);
 	}
 }
 
