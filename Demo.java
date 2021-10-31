e1effa32f2a3d4ee49abee1ec3624e2de47e8c48
felixvl <digitaleservice@gmail.com>
2021-10-31 11:34:55 +0100

    PA

--- Demo.java
+++ Demo.java
@@ -1,42 +1,20 @@
-class Demo{   // OP
+class Demo{   // PA
 	final static public void main(String[] ab) { 
-		HereWeGo go = new Rechthoek();
-		go.hup();
-		go.gaan();
-		System.out.println(go.a);
-		Vierkant r = (Vierkant)go;
-		r.gaan();
-		System.out.println(r.a);
-		Rechthoek rr = (Rechthoek)r;
-		rr.hup();
+		System.out .println("start");
+		AAA a = new QQQ();
+		AAA b = (AAA)a;
+		System.out.println("einde");
 	}
 }
 
+class AAA{
 
+}
 
-abstract class HereWeGo{
-	int a = 3;
-	abstract void gaan();
-	static void hup(){
-		System.out.println("hup");
-	}
+class QQQ extends AAA{
+
 }
-abstract class Rond extends HereWeGo{
-	int a = 4;
-	void gaan(){
-		System.out.println("we zijn aan het gaan");
-	}
-}
-abstract class Vierkant extends Rond{
-	int a = 5;
-	abstract void gaan();
-}
-class Rechthoek extends Vierkant{
-	int a = 6;
-	void gaan(){
-		System.out.println("gaan bij rechthoek");
-	}
-	static void hup(){
-		System.out.println("huphup");
-	}
+
+interface JJJ{
+
 }
