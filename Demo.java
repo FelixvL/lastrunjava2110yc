e98cf534453acbb8ad01de629c7c99919e434159
felixvl <digitaleservice@gmail.com>
2021-10-31 12:25:59 +0100

    PC

--- Demo.java
+++ Demo.java
@@ -1,9 +1,9 @@
-class Demo{   // QB
+class Demo{   // QC
 	final static public void main(String[] ab){ 
 		int a = 'e';
 		char c = (char)a;
 		long d = new Long(a);
-		float e = new Float(25.3);
+		float e = 25.3;
 		System.out.println(d);
 	}
 }
