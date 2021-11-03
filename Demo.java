d78ee2238559ecad1e79479cc7ef244a17e5c443
felixvl <digitaleservice@gmail.com>
2021-11-03 16:59:45 +0100

    WE

--- Demo.java
+++ Demo.java
@@ -1,13 +1,23 @@
-class Demo{   //  WD
+class Demo{   //  WE
 	final static public void main(String[] ab){
-		lopen();
-		lopen(3);
+		lopen(new VB().gaan(true));
 		System.out.println("einde");
 	}
-	void lopen(){
+	static void lopen(double v){
 		System.out.println("lopen1");
 	}
-	void lopen(int a){
+	static void lopen(int a){
 		System.out.println("lopen2");		
+	}
+}
+
+class VB{
+	int gaan(boolean t){
+		System.out.println("lopen3");
+		return 7;
+	}
+	float gaan(char w){
+		System.out.println("lopen4");
+		return 6.3;
 	}
 }
