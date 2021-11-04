daadab960ba10dff564c036bf9d193daa07141ba
felixvl <digitaleservice@gmail.com>
2021-11-04 18:09:38 +0100

    XA

--- Demo.java
+++ Demo.java
@@ -1,33 +1,9 @@
-class Demo{   //  WW
+class Demo{   //  XA
 	final static public void main(String[] ab){
-		Fiets fiets = new Fiets();
-		try{
-			fiets.fietsen();
-		}catch(BelKapotE be){
-			System.out.println("bel erg kapot");
-		}catch(Exception e){
-			System.out.println("bel kapot");
-		}finally{
-			System.out.println("a");
-		}
-		System.out.println("b");
+		int a = 34;
+		char b = (char)a;
+		b++;
+		System.out.println(a);
+		System.out.println("Einde");
 	}
 }
-
-class Fiets{
-	Bel bel = new Bel();
-	void fietsen() throws BelKapotE{
-		bel.bellen();
-		System.out.println("ik fiets");
-	}
-
-}
-
-class Bel{
-	void bellen()throws BelKapotE{
-		System.out.println("ik ben aan het bellen");
-		throw new BelKapotE();
-	}
-} 
-
-class BelKapotE extends Exception{}
