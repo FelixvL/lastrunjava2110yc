241a8d17a0497d259dd549b63cb7f4faab9576ac
felixvl <digitaleservice@gmail.com>
2021-11-04 18:16:14 +0100

    XD

--- Demo.java
+++ Demo.java
@@ -1,20 +1,17 @@
-class Demo{   //  XC
+class Demo{   //  XD
 	final static public void main(String[] ab){
-		int a = 34;
-		char b = (char)a;
-		b++;
-		System.out.println(a);
 		Trial q = new Trial();
 		Trial w = q;
 		q.a++;
 		System.out.println(w.b);
+		new Trial();
 		System.out.println("Einde");
 	}
 }
 class Trial{
 	static b = abc();
 	int a = 22;
-	int abc(){
+	static int abc(){
 		System.out.println("ff checken");
 		return a++;
 	}
