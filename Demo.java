5dd33a9f178300a891a7fb18a8f5e7f4a46ab23a
felixvl <digitaleservice@gmail.com>
2021-11-04 18:40:49 +0100

    XG bewijs

--- Demo.java
+++ Demo.java
@@ -1,10 +1,10 @@
 class Demo{   //  XG
 	final static public void main(String[] ab){
-		Trial q = new Trial();
+		Trial q = new Trial();//22 23
 		Trial w = q;
-		q.a++;
-		Trial z = new Trial();
-		Trial zz = new Trial();
+		q.a++; //24
+		Trial z = new Trial();//24 25
+		Trial zz = new Trial(); //25 26
 		System.out.println(w.b);
 		System.out.println(w.a);
 		System.out.println("Einde");
@@ -20,7 +20,7 @@ class Trial{
 	}
 	 int abc(){
 		System.out.println("ff checken");
-		return a++;
+		return ++a;
 	}
 	 static int a = 22;
 	 int b = abc();
