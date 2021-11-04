93a5b9608b75658b4fb67f0f7ce0583a2697210c
felixvl <digitaleservice@gmail.com>
2021-11-04 18:27:39 +0100

    XF

--- Demo.java
+++ Demo.java
@@ -1,18 +1,22 @@
-class Demo{   //  XE
+class Demo{   //  XF
 	final static public void main(String[] ab){
 		Trial q = new Trial();
 		Trial w = q;
 		q.a++;
+		new Trial();
+		new Trial();
 		System.out.println(w.b);
 		System.out.println(w.a);
-		new Trial();
 		System.out.println("Einde");
 	}
 }
 class Trial{
-	static int b = abc();
-	static int a = 22;
-	static int abc(){
+	 int b = abc();
+	 int a = 22;
+	Trial(){
+		System.out.println("hoi");
+	}
+	 int abc(){
 		System.out.println("ff checken");
 		return a++;
 	}
