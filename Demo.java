04d7a655fccb18a47a6a36040ada9a1c368cc7d2
felixvl <digitaleservice@gmail.com>
2021-11-04 18:48:26 +0100

    XI

--- Demo.java
+++ Demo.java
@@ -1,24 +1,7 @@
-class Demo{   //  XH
+class Demo{   //  XI
 	final static public void main(String[] ab){
-		Trial q = new Trial();//22 23
-		System.out.println(q.abc());
-		System.out.println(q.abc());
-		System.out.println(q.abc());
-		System.out.println(q.abc());
-		System.out.println(q.b);
-		System.out.println(q.a);
+		int[] a = {{},{},{},null};
+		System.out.println(a[0].length);
 		
 	}
-}
-class Trial{
-	Trial(){
-		System.out.println("hoi");
-	}
-	 int abc(){
-		System.out.println("ff checken");
-		return a++;
-	}
-	 static int a = 23;
-	 int b = abc();
-
 }
