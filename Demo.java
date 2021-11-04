99fcb2d4f0c9189d236a34cd57b5985fc19a5084
felixvl <digitaleservice@gmail.com>
2021-11-04 18:11:58 +0100

    XB

--- Demo.java
+++ Demo.java
@@ -1,9 +1,16 @@
-class Demo{   //  XA
+class Demo{   //  XB
 	final static public void main(String[] ab){
 		int a = 34;
 		char b = (char)a;
 		b++;
 		System.out.println(a);
+		Trial q = new Trial();
+		Trial w = q;
+		q.a++;
+		System.out.println(w.a);
 		System.out.println("Einde");
 	}
+}
+class Trial{
+	int a = 22;
 }
