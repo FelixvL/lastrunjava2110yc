732b36f979a5d450098ae78e501b0b7515d4b72a
felixvl <digitaleservice@gmail.com>
2021-11-04 18:18:12 +0100

    XE

--- Demo.java
+++ Demo.java
@@ -1,16 +1,17 @@
-class Demo{   //  XD
+class Demo{   //  XE
 	final static public void main(String[] ab){
 		Trial q = new Trial();
 		Trial w = q;
 		q.a++;
 		System.out.println(w.b);
+		System.out.println(w.a);
 		new Trial();
 		System.out.println("Einde");
 	}
 }
 class Trial{
-	static b = abc();
-	int a = 22;
+	static int b = abc();
+	static int a = 22;
 	static int abc(){
 		System.out.println("ff checken");
 		return a++;
