3084e4ad47e4ebc74f2bedaf807f1997c4de06b9
felixvl <digitaleservice@gmail.com>
2021-11-10 18:05:24 +0100

    ZA

--- Demo.java
+++ Demo.java
@@ -1,7 +1,5 @@
-class Demo{   //  XK
-	final static public void main(String[] ab){
-		int[][][] a = {{3,8,5,2},{33,44,55,66},{1,2},null};
-		System.out.println(a[1].length);
-		
+class Demo{  // ZA
+	public static void main(String[] args){
+		System.out.println("go");
 	}
 }
