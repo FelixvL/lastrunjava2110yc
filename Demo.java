6c606ee73fa2afeb6d2ac37da8e84692ff9ca73c
felixvl <digitaleservice@gmail.com>
2021-11-13 10:44:12 +0100

    ZF

--- Demo.java
+++ Demo.java
@@ -1,8 +1,8 @@
-class Demo{  // ZE
+class Demo{  // ZF
 	public static void main(String[] args){
 		System.out.println("go");
 		int a = 3;
-		GGG fff = new GGG();
+		GGG fff = new HHH();
 		fff.fff(a);
 		System.out.println("e");
 	}
@@ -11,10 +11,21 @@ class Demo{  // ZE
 class GGG{
 	int a = 6;
 	void fff(int a){
+		System.out.println("vlakbij");
 		System.out.println(a);
 		bbb(a);
+		fff();
 	}
 	void bbb(double g){
+		System.out.println("daar");
 		System.out.println(a);
 	}
+}
+class HHH extends GGG{
+	int a = 2;
+	void fff(){
+		System.out.println("hier");
+		System.out.println(a);
+	}
+
 }
