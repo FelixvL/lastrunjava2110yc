16dffc9e2038c9fb010b9e063022ffb74333697a
felixvl <digitaleservice@gmail.com>
2021-11-13 10:36:09 +0100

    ZB

--- Demo.java
+++ Demo.java
@@ -1,5 +1,16 @@
-class Demo{  // ZA
+class Demo{  // ZB
 	public static void main(String[] args){
 		System.out.println("go");
+		int a = 3;
+		GGG fff = new GGG();
+		fff.FFF(a);
+		System.out.println("e");
+	}
+}
+
+class GGG{
+	int a = 6;
+	void FFF(int a){
+		System.out.println(a);
 	}
 }
